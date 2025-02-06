07d06fc760a6d5ebdec7e01377ab11084cfbc823
Kumar Satyanshu <satyanarpit05@gmail.com>
2025-02-06 20:26:30 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Project SwasthyaAI',
+    version= '0.0.0',
+    author= 'Kumar Satyanshu',
+    author_email= 'satyanarpit05@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
